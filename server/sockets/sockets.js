37e93e190a77095dc11f259d50379cda6d67d562
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-08-29 17:44:52 +0500

    Updated

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://mern-stack-chat-application-flax.vercel.app',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
